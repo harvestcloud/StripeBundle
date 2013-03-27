1c099e5b7068ff51e41fc13eda1736ca7eb2cdac
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2013-03-26 20:24:44 -0400

    Updated Stripe to accept publishable_key as second argument. Updated services.yml accordingly

--- Stripe.php
+++ Stripe.php
@@ -18,13 +18,59 @@ namespace HarvestCloud\StripeBundle;
 class Stripe
 {
     /**
+     * secret_key
+     *
+     * @var string
+     */
+    protected $secret_key;
+
+    /**
+     * publishable_key
+     *
+     * @var string
+     */
+    protected $publishable_key;
+
+    /**
      * __construct
      *
      * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
      * @since  2013-03-26
+     *
+     * @param  string $secret_key
+     * @param  string $publishable_key
      */
-    public function __construct($secret_key)
+    public function __construct($secret_key, $publishable_key)
     {
-        \Stripe::setApiKey($secret_key);
+        $this->secret_key      = $secret_key;
+        $this->publishable_key = $publishable_key;
+
+        \Stripe::setApiKey($this->secret_key);
+    }
+
+    /**
+     * getSecretKey()
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2013-03-26
+     *
+     * @return string
+     */
+    public function getSecretKey()
+    {
+        return $this->secret_key;
+    }
+
+    /**
+     * getPublishableKey()
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2013-03-26
+     *
+     * @return string
+     */
+    public function getPublishableKey()
+    {
+        return $this->publishable_key;
     }
 }
